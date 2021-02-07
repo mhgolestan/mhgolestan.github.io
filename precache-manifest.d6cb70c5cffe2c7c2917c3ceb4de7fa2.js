4d586a05d84695e376d7679ad9d9cfcf228b6801
Mohammad Hossein Golestan <mohammad.h.golestan@ntnu.no>
2021-02-07 19:28:10 +0100

    Updates

---
+++ precache-manifest.d6cb70c5cffe2c7c2917c3ceb4de7fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a2ddd226bd6ca41574b342b54a2265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a99850f71d86bbf9694f",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a3fb3d2cfe7a7d718803",
+    "url": "/static/js/2.370adc3c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.370adc3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99850f71d86bbf9694f",
+    "url": "/static/js/main.8c3ec78e.chunk.js"
+  },
+  {
+    "revision": "f6b3cb618e80f669c8c3",
+    "url": "/static/js/runtime-main.442f549f.js"
+  }
+]);
